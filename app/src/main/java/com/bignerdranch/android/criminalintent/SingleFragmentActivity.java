27f595d3f23a9e9c159581b6ab6f54985ccba5bd
ermattt <53094167+ermattt@users.noreply.github.com>
2019-08-19 11:39:22 -0400

    Update SingleFragmentActivity.java

--- app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// feedback, please!
+
 abstract public class SingleFragmentActivity
         extends AppCompatActivity
 {
